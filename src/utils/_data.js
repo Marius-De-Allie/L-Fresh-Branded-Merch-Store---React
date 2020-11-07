2e996872f8c44ecf619a837754f8af719d303911
Marius-De-Allie <spectre4300s@yahoo.com>
2020-11-07 18:46:18 -0500

    Create './utils/_data.js file to keep track of all category items

---
+++ src/utils/_data.js
@@ -0,0 +1,20 @@
+export default (function () {
+
+  return {
+    hoodies: {
+      name: 'Hoodies',
+      imgUrl: 'https://images.pexels.com/photos/1482476/pexels-photo-1482476.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    },
+    shirts: {
+      name: 'Shirts',
+      imgUrl: 'https://images.pexels.com/photos/1482476/pexels-photo-1482476.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    },
+    headWear: {
+      name: 'Headwear',
+      imgUrl: 'https://images.pexels.com/photos/1482476/pexels-photo-1482476.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    }
+  };
+})();
+
+
+
