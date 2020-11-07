ca61bbd0c4e998947a9611e47b969a9bd4b89c91
Marius-De-Allie <spectre4300s@yahoo.com>
2020-11-07 18:18:07 -0500

    Create SignInPage component and add SignIn Route component to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './HomePage';
+import SignInPage from './SignInPage';
 
 import '../App.css';
 
@@ -8,7 +9,7 @@ function App() {
     <div className="App">
       <Switch>
         <Route exact path="/" component={HomePage} />
-          
+        <Route path="/signin" component={SignInPage} />
       </Switch>
     </div>
   );
---
+++ src/components/SignInPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SignInPage = (props) => (
+  <div>
+    SIGNIN PAGE
+  </div>
+);
+
+export default SignInPage;
