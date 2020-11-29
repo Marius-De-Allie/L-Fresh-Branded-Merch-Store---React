c329a9b697a19f9f38d85e6a90141725ebb3798d
Marius-De-Allie <spectre4300s@yahoo.com>
2020-11-29 12:50:23 -0500

    Render Nav component within App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './HomePage';
 import SignInPage from './SignInPage';
+import Nav from './Nav';
 
 import '../App.css';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/signin" component={SignInPage} />
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 const Nav = () => (
   <nav className="nav-bar">
     <NavLink to="/">Home</NavLink>
-    <NavLink>Shop</NavLink>
+    <NavLink to="/">Shop</NavLink>
     <NavLink to="/signin">Sign In</NavLink>
   </nav>
 );
