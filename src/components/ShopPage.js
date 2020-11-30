ad4a31d48d7b637ec68623b90cc995bac2a86bca
Marius-De-Allie <spectre4300s@yahoo.com>
2020-11-30 00:40:27 -0500

    Create ShopPage component

---
+++ src/components/ShopPage.js
@@ -0,0 +1,27 @@
+import React from 'React';
+import CategoryItem from './CategoryItem';
+import categoryData from '../utils/_data';
+
+
+const ShopPage = () => {
+
+  // Convert categories object to an array.
+  const categories = Object.keys(categoryData).map(el => categoryData[el]);
+
+  return (
+    <div className="cat-list" style={{display: 'flex', justifyContent: 'center'}}>
+      <ul style={{width: '100%', padding: '0', display: 'flex', justifyContent: 'space-around'}}>
+        {categories.map(cat => 
+          <CategoryItem 
+            key={cat.name}
+            {...cat} 
+          />
+          )
+        }
+      </ul>
+      
+    </div>
+  );
+};
+
+export default ShopPage;
