a3bfc61f71ef116a394abb0906ce55d6c5739afe
Marius-De-Allie <spectre4300s@yahoo.com>
2020-11-29 22:58:14 -0500

    Declare handleClick function within CategroyItem component and add onClick prop to cat-img element

--- src/components/CategoryItem.js
+++ src/components/CategoryItem.js
@@ -1,20 +1,40 @@
 import React from 'react';
+import { Lin, useHistory } from 'react-router-dom';
 
-const CategoryItem = ({ imgUrl, name }) => console.log(imgUrl)||(
-  <li 
-    className="category-card"
-    style={{listStyle: "none", display: "block", margin: ".25rem"}}
-  >
-    <p>{name}</p>
-    <div className="cat-img" style={{width: "100%"}}>
-      <img 
-        src={imgUrl}
-        alt={name}
-        width="200px"
-        height="300px"
-      />
-    </div>
-  </li>
-);
+const CategoryItem = ({ imgUrl, name }) => {
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push(`/shop/${name}`);
+    console.log('redirect');
+  };
+  
+  
+  
+  console.log(imgUrl);
+  return (
+    <li 
+      className="category-card"
+      style={{listStyle: "none", display: "block", margin: ".25rem"}}
+    >
+      <div 
+        className="cat-img" 
+        style={{width: "100%", cursor: 'pointer'}}
+        onClick={handleClick}
+      >
+        <img 
+          src={imgUrl}
+          alt={name}
+          width="200px"
+          height="300px"
+        />
+        <p>{name}</p>
+        
+        </div>
+    </li>
+
+  );
+}
+
 
 export default CategoryItem;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,32 +3,38 @@ import { NavLink } from 'react-router-dom';
 import { FaShoppingCart } from 'react-icons/fa'
 
 const Nav = () => (
-  <nav className="nav-bar" 
-    style={{
-      border: '1px solid grey',
-      // position: 'fixed',
-      // top: '0px',
-      // left: '0px',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: 'grey',
-      
-    }}
+  <div 
+  style={{border: '1px solid grey',
+        backgroundColor: 'grey'
+      }}
   >
-    <div>
-      <NavLink className="nav-links" to="/">Home</NavLink>
-    </div>
-    <div>
-      <NavLink className="nav-links" to="/">Shop</NavLink>
-    </div>
-    <div>
-      <NavLink className="nav-links" to="/signin">Sign In</NavLink>
-    </div>
-    <div style={{cursor: 'pointer', justifySelf: 'flex-end'}} >
-      <FaShoppingCart color="white" />
-    </div>
-  </nav>
+    <nav className="nav-bar" 
+      style={{
+        // position: 'fixed',
+        // top: '0px',
+        // left: '0px',
+        display: 'flex',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        maxWidth: '900px',
+        margin: '0 auto'
+        
+      }}
+    >
+      <div>
+        <NavLink className="nav-links" to="/">Home</NavLink>
+      </div>
+      <div>
+        <NavLink className="nav-links" to="/">Shop</NavLink>
+      </div>
+      <div>
+        <NavLink className="nav-links" to="/signin">Sign In</NavLink>
+      </div>
+      <div style={{cursor: 'pointer', justifySelf: 'flex-end'}} >
+        <FaShoppingCart color="white" />
+      </div>
+    </nav>
+  </div>
 );
 
 export default Nav;
