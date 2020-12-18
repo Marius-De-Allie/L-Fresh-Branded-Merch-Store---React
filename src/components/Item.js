9a88eeb8a8b6e3d441781b3e7e424a1c04020b60
Marius-De-Allie <spectre4300s@yahoo.com>
2020-12-17 23:50:43 -0500

    Declare renderSize function within ItemSize component

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ItemSize from './ItemSize';
 
 const Item = ( { item }) => {
 
@@ -8,7 +9,7 @@ const Item = ( { item }) => {
       <img src={item.imgUrl} alt={item.itemName} style={{width: '275px'}}/>
       <div className="item-txt-container" style={{display: 'flex', justifyContent: 'space-between'}}>
         <div className="size-container" style={{display: 'flex'}}>
-          {item.size.map(size => <p>{size}</p>)}
+          {item.size.map(size => <ItemSize key={size} size={size} />)}
         </div>
         <p>{`price: `}<span style={{fontWeight: 'bold'}}>${item.price}</span></p>
       </div>
--- src/components/ItemSize.js
+++ src/components/ItemSize.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 const ItemSize = ({ size }) => {
 
-  return <p>size</p>
+  // Function that renders text equivalent of size array element.
+  const renderSize = () => {
+    switch(size) {
+      case 'small':
+        return 's';
+      case 'medium':
+        return 'm';
+      case 'large':
+        return 'l';
+      default:
+        return size;
+    }
+  };
+
+  return <p style={{textTransform: 'uppercase', padding: '0 .20rem'}}>{renderSize()}</p>
 };
 
 export default ItemSize;
