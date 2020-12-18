9270b5eb8fe7cbd01fccbefbbe3574a4ed776aad
Marius-De-Allie <spectre4300s@yahoo.com>
2020-12-17 23:01:08 -0500

    Declare array of items within ItemList component

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -2,9 +2,12 @@ import React from 'react';
 import Item from './Item';
 import categoryData from '../utils/_data';
 
-const ItemList = (props) => {
+const ItemList = ({ match }) => {
 
-  const categories = Object.keys(categoryData).map(item => categoryData[item]);
+  let items = Object.keys(categoryData).map(el => categoryData[el]);
+  items = items.filter(item => item.name === match.params.catId)[0].items;
+  console.log('CATITEMS', items);
+  // categories.filter(el => )
 
   return (
     <div>
