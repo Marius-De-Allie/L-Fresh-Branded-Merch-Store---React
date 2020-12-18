a5eeb6b5b8effd065ddb199f6852006cff5a99e3
Marius-De-Allie <spectre4300s@yahoo.com>
2020-12-17 22:41:09 -0500

    Add item property to each object in _data.js file

--- src/utils/_data.js
+++ src/utils/_data.js
@@ -3,18 +3,86 @@ export default (function () {
   return {
     hoodies: {
       name: 'Hoodies',
-      imgUrl: 'https://images.pexels.com/photos/2080734/pexels-photo-2080734.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+      imgUrl: 'https://images.pexels.com/photos/2080734/pexels-photo-2080734.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      items: [
+        {
+          ItemName: 'one',
+          price: 25,
+          size: ['small', 'medium', 'large', 'XL', 'XXL']
+        },
+        {
+          ItemName: 'two',
+          price: 35,
+          size: ['small', 'medium', 'large', 'XL', 'XXL']
+        },
+        {
+          ItemName: 'three',
+          price: 30,
+          size: ['small', 'medium', 'large', 'XL', 'XXL']
+        },
+        {
+          ItemName: 'four',
+          price: 40,
+          size: ['small', 'medium', 'large', 'XL', 'XXL']
+        }
+      ]
     },
     shirts: {
       name: 'Shirts',
-      imgUrl: 'https://images.pexels.com/photos/3140049/pexels-photo-3140049.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+      imgUrl: 'https://images.pexels.com/photos/3140049/pexels-photo-3140049.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      items: [
+        {
+          ItemName: 'five',
+          price: 25,
+          size: ['small', 'medium', 'large', 'XL', 'XXL']
+        },
+        {
+          ItemName: 'six',
+          price: 35,
+          size: ['small', 'medium', 'large', 'XL', 'XXL']
+        },
+        {
+          ItemName: 'seven',
+          price: 30,
+          size: ['small', 'medium', 'large', 'XL', 'XXL']
+        },
+        {
+          ItemName: 'eight',
+          price: 40,
+          size: ['small', 'medium', 'large', 'XL', 'XXL']
+        }
+      ]
     },
     headWear: {
       name: 'Headwear',
-      imgUrl: 'https://images.pexels.com/photos/4142539/pexels-photo-4142539.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+      imgUrl: 'https://images.pexels.com/photos/4142539/pexels-photo-4142539.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      items: [
+        {
+          ItemName: 'nine',
+          price: 25,
+          size: ['small', 'medium', 'large', 'XL', 'XXL']
+        },
+        {
+          ItemName: 'ten',
+          price: 35,
+          size: ['small', 'medium', 'large', 'XL', 'XXL']
+        },
+        {
+          ItemName: 'eleven',
+          price: 30,
+          size: ['small', 'medium', 'large', 'XL', 'XXL']
+        },
+        {
+          ItemName: 'twelve',
+          price: 40,
+          size: ['small', 'medium', 'large', 'XL', 'XXL']
+        }
+      ]
     }
   };
 })();
+
+
 
 
 
