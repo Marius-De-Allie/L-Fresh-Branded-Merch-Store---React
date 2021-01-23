91ae23444993478ab24d4201a9bef2aba88fc46b
Marius-De-Allie <spectre4300s@yahoo.com>
2021-01-22 22:44:23 -0500

    Add SET_PRODUCTS case to productsReducer switch statement

--- src/redux/reducers/products.js
+++ src/redux/reducers/products.js
@@ -1,16 +1,14 @@
-
-const initialState = {
-
-};
+import { SET_PRODUCTS } from '../actions/products';
+const initialState = {};
 
 const productsReducer = (state = initialState, action) => {
 
   switch(action.type) {
-
+    case SET_PRODUCTS:
+      return action.products
     default:
       return state;
   };
-
 };
 
 export default productsReducer;
