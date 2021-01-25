aab80d52926f81e61b2710c4eab317204c5e79c8
Marius-De-Allie <spectre4300s@yahoo.com>
2021-01-25 00:18:05 -0500

    Add case logic for ADD_TO_CART action type to the redux cart reducer

--- src/redux/actions/cart.js
+++ src/redux/actions/cart.js
@@ -2,7 +2,7 @@ const ADD_TO_CART = 'ADD_TO_CART';
 
 const addToCart = (cartObj) => ({
   type: ADD_TO_CART,
-  cartObj
+  cartData: {...cartObj}
 });
 
 export {
--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -1,3 +1,4 @@
+import { ADD_TO_CART } from '../actions/cart';
 
 const initialState = {
   items: [],
@@ -6,7 +7,23 @@ const initialState = {
 
 const cartReducer = (state = initialState, action) => {
   switch(action.type) {
+    case ADD_TO_CART:
+      let newCartItem, matchingItem, newItems;
+      // get array index of newly added cart item.
+      const cartItemIdx = state.items.findIndex(item => item.id === action.cartData.id);
 
+      if(cartItemIdx === -1) {
+        newCartItem = action.cartData;
+        newItems = [...state.items, newCartItem];
+      } else {
+        matchingItem = state.items[cartItemIdx];
+        newCartItem = {...matchingItem, quantity: matchingItem.quantity + action.cartData.quantity};
+        newItems = state.items.map((item, idx) => idx === cartItemIdx ? newCartItem : item);
+      }
+      return {
+        ...state,
+        items: newItems
+      }
     default:
       return state;
   }
