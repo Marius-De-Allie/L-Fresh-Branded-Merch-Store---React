d4d3e5afc4923076c439fbd987affc7b9f2ec7ee
Marius-De-Allie <spectre4300s@yahoo.com>
2021-01-24 23:22:34 -0500

    Create redux cart reducer and add cart property to root reducer

---
+++ src/redux/reducers/cart.js
@@ -0,0 +1,15 @@
+
+const initialState = {
+  items: [],
+  total: 0
+};
+
+const cartReducer = (state = initialState, action) => {
+  switch(action.type) {
+
+    default:
+      return state;
+  }
+};
+
+export default cartReducer;
