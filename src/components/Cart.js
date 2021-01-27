49832317aa8425a74e55bb823447f31051a4e7f0
Marius-De-Allie <spectre4300s@yahoo.com>
2021-01-27 00:19:15 -0500

    Loop over cart.items array and render a tabel row for each cart item in array, within Cart component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,22 +1,35 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const Cart = () => (
-  <div style={{width: '900px', margin: '20px auto', display: 'flex', justifyContent: 'center'}}>
-    <table>
-    <thead>
-      <tr>
-        <th>image</th>
-        <th>Name</th>
-        <th>Quantity</th>
-        <th>Price</th>
-        <th>remove</th>
-      </tr>
-    </thead>
-    <tbody>
-     
-    </tbody>
-    </table>
-  </div>
-);
+const Cart = () => {
+  const cart = useSelector(({ cart }) => cart);
+
+  return (
+    <div style={{width: '900px', margin: '20px auto', display: 'flex', justifyContent: 'center'}}>
+      <table>
+      <thead>
+        <tr>
+          <th>image</th>
+          <th>Name</th>
+          <th>Quantity</th>
+          <th>Price</th>
+          <th>remove</th>
+        </tr>
+      </thead>
+      <tbody>
+        {cart.items.map(cartItem => (
+          <tr>
+            <td></td>
+            <td>{cartItem.name}</td>
+            <td>{cartItem.quantity}</td>
+            <td>{cartItem.totalPrice}</td>
+            <td>X</td>
+          </tr>
+        ))}
+      </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default Cart;
